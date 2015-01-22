b97c2123fef5f4d889091c2e5589b06419f3e303
Henrik Engstrom <henrik.engstrom@gmail.com>
2015-01-22 15:08:29 -0500

    Upgrades to latest and greatest

--- project/Depedencies.scala
+++ project/Depedencies.scala
@@ -8,15 +8,15 @@ object Dependencies {
 
   val analyticsLibs = Seq(
     "com.typesafe.trace"   % "trace-collect211"   % "0.1.3",
-    "org.codehaus.jackson" % "jackson-core-asl"   % "1.9.9",
-    "org.codehaus.jackson" % "jackson-mapper-asl" % "1.9.9",
-    "com.typesafe.akka"    %% "akka-actor"        % "2.3.3",
-    "io.spray"             %% "spray-can"         % "1.3.1",
+    "org.codehaus.jackson" % "jackson-core-asl"   % "1.9.13",
+    "org.codehaus.jackson" % "jackson-mapper-asl" % "1.9.13",
+    "com.typesafe.akka"    %% "akka-actor"        % "2.3.9",
+    "io.spray"             %% "spray-can"         % "1.3.2",
     "ch.qos.logback"       % "logback-classic"    % "1.0.13",
 
-    "com.typesafe.akka"    %% "akka-testkit"      % "2.3.3"          % "test",
+    "com.typesafe.akka"    %% "akka-testkit"      % "2.3.9"          % "test",
     "junit"                % "junit"              % "4.5"            % "test",
-    "org.scalatest"        %% "scalatest"         % "2.1.6"          % "test")
+    "org.scalatest"        %% "scalatest"         % "2.2.1"          % "test")
 
   def analyticsDependencies: Seq[Setting[_]] =
     Seq(
